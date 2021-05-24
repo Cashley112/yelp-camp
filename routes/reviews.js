4a4d6fcb4dbaee1dc7d3d8001a8156fdd9793e40
Connor Ashley <ashleycm@vcu.edu>
2021-05-24 15:09:57 -0400

    refactored user routes logic

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 const catchAsync = require('../utils/catchAsync');
 const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware.js');
-
 const reviews = require('../controllers/reviews');
 
 router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview));
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
-
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
-
 const users = require('../controllers/users');
 
-router.get('/register', users.renderRegisterForm);
-router.post('/register', catchAsync(users.createUser));
+router.route('/register')
+    .get(users.renderRegisterForm)
+    .post(catchAsync(users.createUser))
 
-router.get('/login', users.renderLoginForm);
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
+router.route('/login')
+    .get(users.renderLoginForm)
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
 
 router.get('/logout', users.logout)
 
