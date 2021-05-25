f783d4a32190053e0c5bc8d8719398fb594bff36
Connor Ashley <ashleycm@vcu.edu>
2021-05-25 15:26:03 -0400

    updated edit campground controller to push an array of image objects onto the existing instance of campground model

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -11,7 +11,9 @@ module.exports.renderNewForm = (req, res) => {
 
 module.exports.createCrampground = async (req, res) => {
     const campground = new Campground(req.body.campground);
+    campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.author = req.user._id;
+    console.log(campground);
     await campground.save();
     req.flash('success', 'Successfully made a new campground!');
     res.redirect(`campgrounds/${campground._id}`);
@@ -46,6 +48,9 @@ module.exports.renderEditForm = async (req, res) => {
 module.exports.editCampground = async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
+    campground.images.push(...imgs);
+    await campground.save();
     req.flash('success', 'Successfully updated a campground!');
     res.redirect(`/campgrounds/${campground._id}`);
 };
